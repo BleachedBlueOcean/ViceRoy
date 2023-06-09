da9cb1186ef9420eb66da365abde46b7a7d10b8e
Matthew Baseman <matthew.baseman@gmail.com>
2023-06-09 15:38:59 -0700

    Corrected bug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import Trading from './trading_page/Trading.jsx'
-
 import '../css/App.css'
 import InitialPage from './initial_page/InitialPage.jsx';
-import Trading from './trading_page/Trading.jsx';
 import UserProfile from './user_profile/UserProfile.jsx';
 // import InitialPage from './initial_page/InitialPage.jsx';
 
 function App(props) {
-  const [view, setView] = useState("initial")
-
-  const renderView = () => {
-    switch (view) {
-      case "initial":
-        return (
-          <div>
-            <h1>Yuh!</h1>
-          </div>
-        )
-    }
-  }
 
   const [view, setView] = useState("default");
 
