242f9213966885b86aaf9ad411063ce9af9034e7
Noriel <flores1noriel@gmail.com>
2023-06-10 15:35:30 -0700

    Rendered out news list. Added news API global variable

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    cors: { origin: "*" },
+    proxy: {
+      '/api': {
+        target: 'https://cryptopanic.com/api/v1/posts/',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  }
 })
